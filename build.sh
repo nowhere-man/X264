1194629826297f8e6698b7b79980b2bd53683b4f
nowhere-man <liu.shaojie@outlook.com>
2024-04-29 23:20:03 +0800

    add 2 yuv:foreman and suzie

--- build.sh
+++ build.sh
@@ -1,7 +1,24 @@
+#!/bin/bash
+
 rm -rf build
 mkdir -p build
 
-./configure --prefix=./build --enable-shared --bit-depth=8 --chroma-format=420 --disable-opencl --disable-interlaced --enable-debug
+./configure \
+    --prefix=./build \
+    --enable-shared \
+    --bit-depth=8 \
+    --chroma-format=420 \
+    --disable-opencl \
+    --disable-interlaced \
+    --disable-asm \
+    --disable-thread \
+    --disable-avs \
+    --disable-swscale \
+    --disable-lavf \
+    --disable-ffms \
+    --disable-gpac \
+    --disable-lsmash \
+    --enable-debug \
 
 make clean
 
